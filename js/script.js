a246b2d6bbc662751ddb1491bfe2e07081487d15
truboprovod4uk <truboprovod4uk@gmail.com>
2018-10-31 16:36:51 +0200

    third commit

--- js/script.js
+++ js/script.js
@@ -1,39 +1,43 @@
-// Event handling
-document.addEventListener("DOMContentLoaded",
-  function (event) {
-    
+
+    var htmlPlants="snippets/plants-snippet.html";
+    var htmlPlantsLoop="snippets/plants-snippet-loop.html";
+    var htmlFishes="snippets/fishes-shippet.html";
+    var htmlGalery="snippets/gallery-snippet.html";
+    var htmlHome="snippets/home-snippet.html";
+    var urlPlants="data/plants.json";
+    var contcont="<div class='container-content'>";
 
 
-        // Call server to get the name
-        /*$ajaxUtils
-          .sendGetRequest("data/plants.json", 
-            function (res) {
-              var reult="";
-                for (var i=0; i<res.length; i++){
-              if (i%2==0){
-                var float="<div class='plant-left'>";
-              }else{
-                var float="<div class='plant-right'>"
-              }
-              var foto = "<img src="+ res[i].foto + " alt='aqua foto'>" ;
-              var message = "<p>" + res[i].plantName + "</p>" + "<p>" + res[i].description+"</p></div>";
-              reult+=float+foto+message;
-              }
-              document.querySelector("#plants")
-                .innerHTML = "<p>" + reult + "</p>" ;
-            });*/
+// Convenience function for inserting innerHTML for 'select'
+var insertHtml = function (selector, html) {
+  var targetElem = document.querySelector(selector);
+  targetElem.innerHTML = html;
+};
 
-        // Call server to get the name
+
+// Return substitute of '{{propName}}'
+// with propValue in given 'string'
+var insertProperty = function (string, propName, propValue) {
+  var propToReplace = "{{" + propName + "}}";
+  string = string
+    .replace(new RegExp(propToReplace, "g"), propValue);
+  return string;
+}
+
+
+    var loadPlants = function(){
+
         $ajaxUtils
-          .sendGetRequest("data/plants.json", 
+          .sendGetRequest(urlPlants, 
             function (res) {
               var reult="";
                 for (var i=0; i<res.length; i++){
               if (i%2==0){
                 var float="<div class='plant-left'>";
               }else{
-                var float="<div class='plant-right'>"
+                var float="<div class='plant-right'>";
               }
+
               var foto = "<img src="+ res[i].foto + " alt='aqua foto'>" ;
               var name = "<h3>" + res[i].plantName + "</h3>";
               var pH="<ul>"+"<li>"+"Water pH: "+res[i].pH+"</li>";
@@ -43,11 +47,48 @@ document.addEventListener("DOMContentLoaded",
               var co2="<li>"+"CO2: "+res[i].CO2+"</li></ul></div>";
               reult+=float+foto+name+pH+temp+hardness+lighting+co2;
               }
-              document.querySelector("#plants")
-                .innerHTML =  reult  ;
+              document.querySelector("#main-content")
+                .innerHTML = contcont+reult+"</div>";
             });
+    }
 
+
+    function loadPlants2(urlPlants,
+                        htmlPlantsLoop) {
+
+  var plants=urlPlants;
+
+  for (var i = 0; i < urlPlants.length; i++) {
+    // Insert category values
+    var html = htmlPlantsLoop;
+    var name = plants[i].plantName;
+    var pH = plants[i].pH
+    var hardness = plants[i].hardness;
+    var lighting = plants[i].lighting;
+    var co2 = plants[i].co2;
+    html = insertProperty(html, "name", name);
+    html = insertProperty(html, "pH", pH);
+    html = insertProperty(html, "hardness", hardness);
+    html = insertProperty(html, "lighting", lighting);
+    html = insertProperty(html, "co2", co2);
+
+    finalHtml += html;
   }
-);
+
+  finalHtml ;
+  return finalHtml;
+}
+   
+
+var loadHome = function(){
+
+$ajaxUtils.sendGetRequest(
+  htmlHome,
+  function (res) {
+    document.querySelector("#main-content").innerHTML = res;
+
+  },
+  false);
 
 
+}
