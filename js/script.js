519048b549cd88bd9ba68c31492b20486546ad6a
truboprovod4uk <truboprovod4uk@gmail.com>
2018-10-31 20:12:08 +0200

    third commit

--- js/script.js
+++ js/script.js
@@ -34,66 +34,12 @@ $ajaxUtils.sendGetRequest(
 }
 
 
-    var loadPlants = function(){
-
-        $ajaxUtils
-          .sendGetRequest(urlPlants, 
-            function (res) {
-              var reult="";
-                for (var i=0; i<res.length; i++){
-              if (i%2==0){
-                var float="<div class='plant-left'>";
-              }else{
-                var float="<div class='plant-right'>";
-              }
-
-              var foto = "<img src="+ res[i].foto + " alt='aqua foto'>" ;
-              var name = "<h3>" + res[i].plantName + "</h3>";
-              var pH="<ul>"+"<li>"+"Water pH: "+res[i].pH+"</li>";
-              var temp="<li>"+"Temperature: "+res[i].temperature+"</li>";
-              var hardness="<li>"+"Hardness: "+res[i].hardness+"</li>";
-              var lighting="<li>"+"Lighting: "+res[i].lighting+"</li>";
-              var co2="<li>"+"CO2: "+res[i].CO2+"</li></ul></div>";
-              reult+=float+foto+name+pH+temp+hardness+lighting+co2;
-              }
-              document.querySelector("#main-content")
-                .innerHTML = contcont+reult+"</div>";
-            });
-    }
-
-
-    function loadPlants2(urlPlants,
-                        htmlPlantsLoop) {
-
-  var plants=urlPlants;
-
-  for (var i = 0; i < urlPlants.length; i++) {
-    // Insert category values
-    var html = htmlPlantsLoop;
-    var name = plants[i].plantName;
-    var pH = plants[i].pH
-    var hardness = plants[i].hardness;
-    var lighting = plants[i].lighting;
-    var co2 = plants[i].co2;
-    html = insertProperty(html, "name", name);
-    html = insertProperty(html, "pH", pH);
-    html = insertProperty(html, "hardness", hardness);
-    html = insertProperty(html, "lighting", lighting);
-    html = insertProperty(html, "co2", co2);
-
-    finalHtml += html;
-  }
-
-  finalHtml ;
-  return finalHtml;
-}
-   
 
 /*--------------------------------------------------*/
 // Load the menu categories view
-var loadPlants2 = function () {
+var loadPlants = function () {
   $ajaxUtils.sendGetRequest(
-    urlPlants/*json*/,
+    urlPlants,/*json*/
     buildAndShowCategoriesHTML);
 };
 
@@ -102,10 +48,8 @@ var loadPlants2 = function () {
 // from the server
 function buildAndShowCategoriesHTML (categories/*json(обэкт що буде оброблятись)*/) {
 
-
       // Retrieve single category snippet
-      $ajaxUtils.sendGetRequest(
-        categoryHtml,/*url до фрагменту який буде лупатись*/
+      $ajaxUtils.sendGetRequest(htmlPlantsLoop,/*url до фрагменту який буде лупатись*/
         function (htmlPlantsLoop) {/*handle фрагменту який буде лупатись*/
 
 
@@ -126,26 +70,28 @@ function buildCategoriesViewHtml(categories,/*json*/
 
   var finalHtml = "<div class='container-content' id='plants'>";
 
-  // Loop over categories
-
-
-  for (var i = 0; i < urlPlants.length; i++) {
-    // Insert category values
+  for (var i = 0; i < categories.length; i++) {
+    if (i%2==0){
+      var float="plant-left";
+    }else{
+      var float="plant-right";
+    }
     var html = htmlPlantsLoop;
-    var name = plants[i].plantName;
-    var pH = plants[i].pH
-    var hardness = plants[i].hardness;
-    var lighting = plants[i].lighting;
-    var co2 = plants[i].co2;
+    var name = categories[i].plantName;
+    var pH = categories[i].pH
+    var temp=categories[i].temperature;
+    var hardness = categories[i].hardness;
+    var lighting = categories[i].lighting;
+    var co2 = urlPlants[i].co2;
+    html = insertProperty(html, "float", float);
     html = insertProperty(html, "name", name);
     html = insertProperty(html, "pH", pH);
+    html = insertProperty(html, "temp", temp);
     html = insertProperty(html, "hardness", hardness);
     html = insertProperty(html, "lighting", lighting);
     html = insertProperty(html, "co2", co2);
-
     finalHtml += html;
   }
-
   finalHtml += "</div>";
   return finalHtml;
 }
